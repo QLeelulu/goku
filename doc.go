f522cabaff22cd37217be948cc4efa0fa19602c4
QLeelulu <qleelulu@gmail.com>
2012-07-15 17:54:36 +0800

    doc

--- doc.go
+++ doc.go
@@ -10,48 +10,48 @@
 // 
 // Example:
 // 
-// package main
+//        package main
 //
-// import (
-//     "github.com/QLeelulu/goku"
-//     "log"
-//     "path"
-//     "runtime"
-// )
+//        import (
+//            "github.com/QLeelulu/goku"
+//            "log"
+//            "path"
+//            "runtime"
+//        )
 //
-// /**
-//  * Controller & Action
-//  */
-// var _ = goku.Controller("home").
-//     Get("index", func(ctx *goku.HttpContext) goku.ActionResulter {
-//     return ctx.View(nil)
-// })
+//        /**
+//         * Controller & Action
+//         */
+//        var _ = goku.Controller("home").
+//            Get("index", func(ctx *goku.HttpContext) goku.ActionResulter {
+//            return ctx.View(nil)
+//        })
 //
-// // routes
-// var routes []*goku.Route = []*goku.Route{
-//     &goku.Route{
-//         Name:    "default",
-//         Pattern: "/{controller}/{action}/",
-//         Default: map[string]string{"controller": "home", "action": "index"},
-//     },
-// }
+//        // routes
+//        var routes []*goku.Route = []*goku.Route{
+//            &goku.Route{
+//                Name:    "default",
+//                Pattern: "/{controller}/{action}/",
+//                Default: map[string]string{"controller": "home", "action": "index"},
+//            },
+//        }
 //
-// // server config
-// var config *goku.ServerConfig = &goku.ServerConfig{Addr: ":8080"}
+//        // server config
+//        var config *goku.ServerConfig = &goku.ServerConfig{Addr: ":8080"}
 //
-// func init() {
-//     // project root dir, this code can not put to main func
-//     _, filename, _, _ := runtime.Caller(1)
-//     config.RootDir = path.Dir(filename)
-// }
+//        func init() {
+//            // project root dir, this code can not put to main func
+//            _, filename, _, _ := runtime.Caller(1)
+//            config.RootDir = path.Dir(filename)
+//        }
 //
-// func main() {
-//     rt := &goku.RouteTable{Routes: routes}
-//     s := goku.CreateServer(rt, nil, config)
+//        func main() {
+//            rt := &goku.RouteTable{Routes: routes}
+//            s := goku.CreateServer(rt, nil, config)
 //
-//     goku.Logger().Logln("Server start on", s.Addr)
-//     log.Fatal(s.ListenAndServe())
-// }
+//            goku.Logger().Logln("Server start on", s.Addr)
+//            log.Fatal(s.ListenAndServe())
+//        }
 //
 package goku
 
